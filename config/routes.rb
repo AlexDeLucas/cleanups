82ecff6187937e92a4188bfe248c4f100e933c04
Alessandra <alex.delucas@gmail.com>
2020-10-07 17:21:40 -0400

    Added new routes to specify cleanups categorized by state, city, and by user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   root 'homes#index'
   devise_for :users
 
-  resources :cleanups
+  resources :cleanups do  
+    resources :states
+      resources :cities
+  end
+
+  resources :users do
+    resources :cleanups
+  end 
 
 end

c407e207a59496c7549e53a845d391361aa9185b
Alessandra <alex.delucas@gmail.com>
2020-10-08 16:00:54 -0400

    Created a first draft of the cleanup submission form.

--- app/controllers/cleanups_controller.rb
+++ app/controllers/cleanups_controller.rb
@@ -6,7 +6,9 @@ class CleanupsController < ApplicationController
 
     # GET /cleanups/new
     def new
-        @cleanup = Cleanup.new 
+        @cleanup = Cleanup.new
+        @state_collection = Cleanup::STATES
+        @cleanup_type_collection = Cleanup::CLEANUP_TYPES 
     end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   
   has_many :cleanups 
 
+  # Are any of these validations actually necessary? Maybe just the role validation? Does Devise take care of the rest?
   validates :email, uniqueness: true
   validates :encrypted_password, presence: true
   validates :role, inclusion: { in: ['user', 'admin'] }

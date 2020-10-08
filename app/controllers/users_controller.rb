afe5b9e9403426a5c57ecaf106eb23446c9167fe
Alessandra <alex.delucas@gmail.com>
2020-10-08 07:28:06 -0400

    Added controller and views for Users; set route for user_cleanups_path and restricted its visibility as a link to only authenticated users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+    # GET /user/cleanups
+    def index
+    end
+
+end 
--- spec/features/user_views_personal_cleanups.spec.rb
+++ spec/features/user_views_personal_cleanups.spec.rb
@@ -10,16 +10,17 @@ feature 'authenticated user views personal cleanups', %Q{
     let!(:cleanup_1) { FactoryBot.create(:cleanup) }
 
     scenario 'see all my cleanups, most recently posted first' do
-        visit cleanups_path
+      login_as(user, :scope => :user)
+      visit user_cleanups_path
     
-        expect(page).to have_content('Lake Champlain Cleanup Social')
-        expect(page).to have_content('Mary Bennett')
-        expect(page).to have_content('07/13/2020')
-        expect(page).to have_content('NY')
-        expect(page).to have_content('Essex')
-        expect(page).to have_content('Coastal')
-        expect(page).to have_content(4)
-        expect(page).to have_content(9)
-        expect(page).to have_content('Most common item was plastic bottles.')
-      end
+      expect(page).to have_content('Lake Champlain Cleanup Social')
+      expect(page).to have_content('Mary Bennett')
+      expect(page).to have_content('07/13/2020')
+      expect(page).to have_content('NY')
+      expect(page).to have_content('Essex')
+      expect(page).to have_content('Coastal')
+      expect(page).to have_content(4)
+      expect(page).to have_content(9)
+      expect(page).to have_content('Most common item was plastic bottles.')
     end
+  end

93cd45e7f94932082d3ffc114a39696ecd2da263
Alessandra <alex.delucas@gmail.com>
2020-10-13 08:36:52 -0400

    Trying to sync up routes and views after a user posts a cleanup. Not working.

--- app/controllers/cleanups_controller.rb
+++ app/controllers/cleanups_controller.rb
@@ -2,6 +2,7 @@ class CleanupsController < ApplicationController
 
     # GET /cleanups
     def index
+        @cleanups = Cleanup.all 
     end
 
     # GET /cleanups/new
@@ -11,15 +12,40 @@ class CleanupsController < ApplicationController
         @cleanup_type_collection = Cleanup::CLEANUP_TYPES 
     end
 
+    # GET /cleanup/1
+    def show
+        @cleanup = Cleanup.find(params[:id])
+    end 
+
     # POST
     def create
-    # Where/how is this object going to be stored? 
     # Once this is created, it needs to be accessible at the user's "My Cleanups" page
     # But the data needs to be retrievable for the public page of cleanups organized by state
     # And it needs to accessible as a public show page
         @cleanup = Cleanup.new(cleanup_params)
         @cleanup.user = current_user
-    
+        if user_signed_in?
+            @cleanup.save
+            flash[:notice] = 'You logged a cleanup!'
+            redirect_to @cleanup
+        else 
+            render action: 'new'
+        end
     end
-    
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def cleanup_params
+        params.require(:cleanup).permit(
+            :user_id, 
+            :title, 
+            :posted_by, 
+            :state, 
+            :city, 
+            :cleanup_type, 
+            :volunteers,
+            :total_trash, 
+            :description,
+            :cleanup_photo
+        )
+    end 
 end

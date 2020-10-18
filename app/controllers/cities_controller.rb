e27fc67563ec7ae70e30d2bb99066473bed8ecb3
Alessandra <alex.delucas@gmail.com>
2020-10-18 17:34:20 -0400

    Fixed issue with string interpolation of state and city names, now they are rendering dynamically when selected by the user.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,6 +3,7 @@ class CitiesController < ApplicationController
     # GET /states/:state_id/cities/:id
     def show
         @cleanups = Cleanup.where(city: params[:id])
+        @city_name = params[:id]
     end
 
 end
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,5 +3,6 @@ class StatesController < ApplicationController
     # GET /states/:id
     def show
         @cleanups = Cleanup.where(state: params[:id]).group_by(&:city)
+        @state_name = params[:id]
     end
 end

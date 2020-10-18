d5e3efd915594384c066230421c278101371f0f8
Alessandra <alex.delucas@gmail.com>
2020-10-18 18:31:05 -0400

    Fixed problem with routing between state and city show pages. However, experiencing issue with getting Back link to function between city page and state page.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,7 +2,7 @@ class StatesController < ApplicationController
 
     # GET /states/:id
     def show
-        @cleanups = Cleanup.where(state: params[:id]).group_by(&:city)
+        @cleanups = Cleanup.where(state: params[:id])
         @state_name = params[:id]
     end
 end

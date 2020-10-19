116636cc7c91fb6b48933da8eb65c06d5746d467
Alessandra <alex.delucas@gmail.com>
2020-10-19 08:01:04 -0400

    Trying to add logic that will enable the list of cities to be grouped according to city name and avoid duplicate listing of cities.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,6 +3,7 @@ class StatesController < ApplicationController
     # GET /states/:id
     def show
         @cleanups = Cleanup.where(state: params[:id])
+        # @cleanups_by_city = @cleanups.group_by(&:city)
         @state_name = params[:id]
     end
 end

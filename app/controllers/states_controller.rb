b2ab8f3cabdc7e1ded04be292b61be84878fcd6a
Alessandra <alex.delucas@gmail.com>
2020-10-20 08:34:25 -0400

    Still trying to get states show page to display only one instance of each state, even if that state has multiple cleanups associated with it.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,7 +2,10 @@ class StatesController < ApplicationController
 
     # GET /states/:id
     def show
-        @cleanups = Cleanup.where(state: params[:id])
+        @cleanups = Cleanup.group('cleanups.id').where(state: params[:id]).group("city")
+        # @cleanups = Cleanup.group('cleanups.id').where(state: params[:id]).order("city")
+        # @cleanups = Cleanup.where(state: params[:id]).group("city")
+        # @cleanups = Cleanup.where(state: params[:id])
         # @cleanups_by_city = @cleanups.group_by(&:city)
         @state_name = params[:id]
     end

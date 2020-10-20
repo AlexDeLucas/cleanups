e1c01cae5d6faeb49df909e79a1171650372f6b5
Alessandra <alex.delucas@gmail.com>
2020-10-20 17:20:21 -0400

    Fixed routing between state and city show pages. Fixed Back button to navigate from city show page back to state show page.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,11 +2,19 @@ class StatesController < ApplicationController
 
     # GET /states/:id
     def show
-        @cleanups = Cleanup.group('cleanups.id').where(state: params[:id]).group("city")
-        # @cleanups = Cleanup.group('cleanups.id').where(state: params[:id]).order("city")
-        # @cleanups = Cleanup.where(state: params[:id]).group("city")
-        # @cleanups = Cleanup.where(state: params[:id])
-        # @cleanups_by_city = @cleanups.group_by(&:city)
+        # Find all cleanups in the state
+        @cleanups = Cleanup.where(state: params[:id])
+        # Find all the cities w/ cleanups in the state
+        @cleanup_cities = @cleanups.map(&:city).uniq
         @state_name = params[:id]
+        
     end
 end
+
+
+        # @cleanups = Cleanup.group('cleanups.id').where(state: params[:id]).group("city")
+        # @cleanups = Cleanup.group('cleanups.id').where(state: params[:id]).order("city")
+        # @cleanups = Cleanup.where(state: params[:id]).group("city")
+        # @cleanups_by_city = @cleanups.group_by(&:city)
+
+        # @uniq_cities = @cleanups_by_city.uniq

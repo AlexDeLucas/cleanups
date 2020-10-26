d22899976a1892e26207ef1cc2ca0212f2d061ec
Alessandra <alex.delucas@gmail.com>
2020-10-26 08:29:51 -0400

    Added pseudocode to cleanups controller index action and the views for cleanups index

--- app/controllers/cleanups_controller.rb
+++ app/controllers/cleanups_controller.rb
@@ -6,6 +6,8 @@ class CleanupsController < ApplicationController
         @state_collection = Cleanup::STATES
         @cleanup = Cleanup.new
         @state_name = params[:id]
+        
+        # When the user selects a state, they are taken to the show page for that state
         if @state_name.select
             redirect_to state_path
         end

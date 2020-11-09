ec52f9aed7abe77d19c9ce98ee0298f6a1dc38fc
Alessandra <alex.delucas@gmail.com>
2020-11-08 20:42:08 -0500

    Deleted some duplicate files

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-    factory :user do
-      sequence(:email) { |n| "ecowarrior#{n}@example.com" }
-      password { 'Earth1234' }
-      password_confirmation { 'Earth1234' }
-      role { 'user' }
-    end
-  end

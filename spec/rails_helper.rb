f9e682ac0102a61e4912cfd071a4e8d7e4c13e48
Alessandra <alex.delucas@gmail.com>
2020-11-11 16:53:12 -0500

    Undoing changes, removing require statement from rails helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,8 +11,6 @@ require File.join(File.dirname(__FILE__), 'support/factory_bot')
 require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require "support/images"
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
